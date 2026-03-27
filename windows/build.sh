79a5949d7c5d48b4233860d5063f09902ea676f8
Giant Pink Robots! <giantpinkrobots@protonmail.com>
2026-03-27 18:32:49 +0100

    Fix icon directory structure for Flatpak

--- windows/build.sh
+++ windows/build.sh
@@ -110,8 +110,8 @@ fi
 cd ../..
 cp -r locale src/dist/variamain/
 mkdir src/dist/variamain/icons/
-cp data/icons/apps/io.github.giantpinkrobots.varia-symbolic.svg src/dist/variamain/icons/
-cp data/icons/apps/io.github.giantpinkrobots.varia.svg src/dist/variamain/icons/
+cp data/icons/symbolic/apps/io.github.giantpinkrobots.varia-symbolic.svg src/dist/variamain/icons/
+cp data/icons/scalable/apps/io.github.giantpinkrobots.varia.svg src/dist/variamain/icons/
 cp -r data/icons/actions/* src/dist/variamain/icons/
 cp -r dependencies_information src/dist/variamain/
 cp ./aria2/$aria2/aria2c.exe src/dist/variamain/
